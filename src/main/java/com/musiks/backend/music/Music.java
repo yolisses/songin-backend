c6dee2f491896d663053620ca21a820da4fed5a3
ulisses <iamurise@gmail.com>
2022-07-17 09:02:57 -0300

    add google login

--- src/main/java/com/musiks/backend/music/Music.java
+++ src/main/java/com/musiks/backend/music/Music.java
@@ -1,8 +1,8 @@
 package com.musiks.backend.music;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @AllArgsConstructor
