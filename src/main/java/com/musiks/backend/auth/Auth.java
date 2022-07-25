3934e843c8cbf64f13eddd77d8df755b2f4937d3
ulisses <iamurise@gmail.com>
2022-07-25 15:31:33 -0300

    refatorate

--- src/main/java/com/musiks/backend/auth/Auth.java
+++ src/main/java/com/musiks/backend/auth/Auth.java
@@ -16,40 +16,36 @@ public class Auth {
     static int sessionWeeksDuration = 1;
     SessionRepo sessionRepo;
 
-    public User getUser(HttpServletRequest req) {
+    private void throwForbidden(String text) {
+        throw new ResponseStatusException(
+                HttpStatus.FORBIDDEN, text
+        );
+    }
+
+    String getSessionId(HttpServletRequest req) {
         Cookie[] cookies = req.getCookies();
-        String sessionId = null;
         for (var cookie : cookies) {
             if (cookie.getName().equals("session_id")) {
-                sessionId = cookie.getValue();
-                break;
+                return cookie.getValue();
             }
         }
-        if (sessionId == null) {
-            throw new ResponseStatusException(
-                    HttpStatus.FORBIDDEN, "Missing session_id cookie"
-            );
-        }
+        throwForbidden("Missing session_id cookie");
+        return null;
+    }
+
+    public User getUser(HttpServletRequest req) {
+        var sessionId = getSessionId(req);
         var session = sessionRepo.findSessionById(sessionId);
-        if (session == null) {
-            throw new ResponseStatusException(
-                    HttpStatus.FORBIDDEN, "Session not found"
-            );
-        }
+        if (session == null)
+            throwForbidden("Session not found");
         var ip = req.getRemoteAddr();
-        if (!session.getIp().equals(ip)) {
-            throw new ResponseStatusException(
-                    HttpStatus.FORBIDDEN, "Invalid origin address to session"
-            );
-        }
+        if (!session.getIp().equals(ip))
+            throwForbidden("Invalid origin address to session");
         var now = ZonedDateTime.now();
         var isExpired = session.getCreatedAt()
                 .plusWeeks(sessionWeeksDuration).isBefore(now);
-        if (isExpired) {
-            throw new ResponseStatusException(
-                    HttpStatus.FORBIDDEN, "Session expired"
-            );
-        }
+        if (isExpired)
+            throwForbidden("Session expired");
         return session.getUser();
     }
 }
