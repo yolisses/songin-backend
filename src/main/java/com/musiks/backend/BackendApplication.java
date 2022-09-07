9f9f8d21eb3d4980b571c731612b0092d0d12c83
Ulisses <iamurise@gmail.com>
2022-09-07 19:13:47 -0300

    add cors domains

--- src/main/java/com/musiks/backend/BackendApplication.java
+++ src/main/java/com/musiks/backend/BackendApplication.java
@@ -27,7 +27,10 @@ public class BackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
                 registry.addMapping("/**")
                         .allowedOrigins("https://sonhin.com")
