57d7e6480e4e5a91ac6ea1be41f8dd15a2150793
Ulisses <iamurise@gmail.com>
2022-09-08 16:21:17 -0300

    remove cors configuration

--- src/main/java/com/musiks/backend/BackendApplication.java
+++ src/main/java/com/musiks/backend/BackendApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @AllArgsConstructor
 @SpringBootApplication
@@ -25,9 +27,19 @@ public class BackendApplication {
     }
 
     @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+    @Bean
     CommandLineRunner commandLineRunner() {
         musicRepo.addNameIndex();
-        boolean runMock = true;
+        boolean runMock = false;
         return args -> {
             if (runMock)
                 mock.run();
