c2ec4d12b9be3427ec88dd0a4230586679aadf39
Ulisses <iamurise@gmail.com>
2022-09-16 15:35:41 -0300

    add user to comments by music query

--- src/main/java/com/sonhin/backend/comment/CommentRepo.java
+++ src/main/java/com/sonhin/backend/comment/CommentRepo.java
@@ -1,12 +1,11 @@
 package com.sonhin.backend.comment;
 
-import com.sonhin.backend.music.Music;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 
 import java.util.List;
 
 public interface CommentRepo extends Neo4jRepository<Comment, Long> {
-    @Query("match (c:Comment)-[:REFERS]->(m:Music) where id(m)=$musicId return c")
+    @Query("match (u:User)<-[:OWNER]-(c:Comment)-[:REFERS]->(m:Music) where id(m)=$musicId return u,c")
     List<Comment> findByMusicId(Long musicId);
 }
