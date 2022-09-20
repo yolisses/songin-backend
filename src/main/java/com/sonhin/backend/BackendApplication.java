98888de102c49d1528ff690d98d084f56785a9a2
Ulisses <iamurise@gmail.com>
2022-09-20 16:07:49 -0300

    lint

--- src/main/java/com/sonhin/backend/BackendApplication.java
+++ src/main/java/com/sonhin/backend/BackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 @AllArgsConstructor
 @SpringBootApplication
 public class BackendApplication {
